717cec60e7e8dadb3840c93db4fed9c1d972da68
arilhr <aurielharlih9213@gmail.com>
2021-03-18 22:41:22 +0700

    First Commit

---
+++ Score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PongSimulation
+{
+    class Score
+    {
+        public static int playerXScore;
+        public static int playerYScore;
+
+        public Score()
+        {
+            playerXScore = 0;
+            playerYScore = 0;
+        }
+    }
+}
